9ba1a3cc624855ea94dcc43caed90c57fd59ca36
Josh <josh@joshlang.ca>
2022-01-19 16:21:01 -0800

    Adding type extensions

---
+++ Epoche.Shared/TypeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Epoche.Shared;
+public static class TypeExtensions
+{
+    public static IEnumerable<Type> GetGenericInterfaces(this Type type, Type genericInterfaceType) =>
+        type is null
+        ? throw new ArgumentNullException(nameof(type))
+        : genericInterfaceType is null
+        ? throw new ArgumentNullException(nameof(genericInterfaceType))
+        : !genericInterfaceType.IsInterface || !genericInterfaceType.IsGenericType
+        ? throw new InvalidOperationException("Parameter must be a generic interface type")
+        : type
+            .GetInterfaces()
+            .Where(x => x.IsGenericType)
+            .Where(x => x.GetGenericTypeDefinition() == genericInterfaceType);
+}
