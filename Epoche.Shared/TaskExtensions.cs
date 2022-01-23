58c1be1de5772fb6a42ba4e00f1903d5e95834d6
Josh <josh@joshlang.ca>
2022-01-22 21:16:22 -0800

    Adding SwallowException

---
+++ Epoche.Shared/TaskExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+namespace Epoche.Shared;
+public static class TaskExtensions
+{
+    public static event Action<Task>? OnException;
+
+#if DEBUG
+    static TaskExtensions()
+    {
+        OnException += task =>
+        {
+            foreach (var e in (task.Exception?.InnerExceptions).EmptyIfNull())
+            {
+                Debug.WriteLine($"Swallowed Exception: {e.GetType().Name} - {e.Message}");
+                Debug.WriteLine(e);
+                Debug.WriteLine("--------");
+            }
+        };
+    }
+#endif
+
+    static void SwallowException(Task task)
+    {
+        _ = task.Exception;
+
+        OnException?.Invoke(task);
+    }
+
+    public static void SwallowExceptions(this Task task) => _ = task.ContinueWith(SwallowException, TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.ExecuteSynchronously);
+
+    public static void SwallowExceptions(this ValueTask task)
+    {
+        if (task.IsCompleted)
+        {
+            return;
+        }
+        task.AsTask().SwallowExceptions();
+    }
+
+    public static void SwallowExceptions<T>(this ValueTask<T> task)
+    {
+        if (task.IsCompleted)
+        {
+            return;
+        }
+        task.AsTask().SwallowExceptions();
+    }
+}
