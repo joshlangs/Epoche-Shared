0b3615c65c831abf58e3903b6ecad6f20e96f5e2
Josh <josh@joshlang.ca>
2022-02-04 09:38:26 -0800

    IListExtensions

---
+++ Epoche.Shared.Tests/IListTextensionTests.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Epoche.Shared;
+public class IListTextensionTests
+{
+    readonly int[] Source = new[] { 1, 2, 3, 4, 5 };
+    IList<int> GetData() => Source.ToList();
+
+    [Fact]
+    [Trait("Type", "Unit")]
+    public void RemoveAll_NoMatch_Returns0() => Assert.Equal(0, GetData().RemoveAll(x => false));
+
+    [Fact]
+    [Trait("Type", "Unit")]
+    public void RemoveAll_AllMatch_Returns5() => Assert.Equal(5, GetData().RemoveAll(x => true));
+
+    [Fact]
+    [Trait("Type", "Unit")]
+    public void RemoveAll_3Match_Returns3() => Assert.Equal(3, GetData().RemoveAll(x => x == 1 || x == 3 || x == 5));
+
+    [Fact]
+    [Trait("Type", "Unit")]
+    public void RemoveAll_NoMatch_ListUnchanged()
+    {
+        var data = GetData();
+        data.RemoveAll(x => false);
+        Assert.Equal(GetData(), data);
+    }
+
+    [Fact]
+    [Trait("Type", "Unit")]
+    public void RemoveAll_AllMatch_ListEmpty()
+    {
+        var data = GetData();
+        data.RemoveAll(x => true);
+        Assert.Empty(data);
+    }
+
+    [Fact]
+    [Trait("Type", "Unit")]
+    public void RemoveAll_3Match_RemainingCorrect()
+    {
+        var data = GetData();
+        data.RemoveAll(x => x == 1 || x == 3 || x == 5);
+        Assert.Equal(new[] { 2, 4 }, data);
+    }
+
+    [Fact]
+    [Trait("Type", "Unit")]
+    public void RemoveFirst_NoMatch_False() => Assert.False(GetData().RemoveFirst(x => false));
+
+    [Fact]
+    [Trait("Type", "Unit")]
+    public void RemoveFirst_AllMatch_True() => Assert.True(GetData().RemoveFirst(x => true));
+
+    [Fact]
+    [Trait("Type", "Unit")]
+    public void RemoveFirst_SingleMatch_True() => Assert.True(GetData().RemoveFirst(x => x == 3));
+
+    [Fact]
+    [Trait("Type", "Unit")]
+    public void RemoveFirst_NoMatch_ListUnchanged()
+    {
+        var data = GetData();
+        data.RemoveFirst(x => false);
+        Assert.Equal(GetData(), data);
+    }
+
+    [Fact]
+    [Trait("Type", "Unit")]
+    public void RemoveFirst_AllMatch_RemoveFirst()
+    {
+        var data = GetData();
+        data.RemoveFirst(x => true);
+        Assert.Equal(new[] { 2, 3, 4, 5 }, data);
+    }
+
+    [Fact]
+    [Trait("Type", "Unit")]
+    public void RemoveFirst_ThirdMatch_RemoveThird()
+    {
+        var data = GetData();
+        data.RemoveFirst(x => x == 3);
+        Assert.Equal(new[] { 1, 2, 4, 5 }, data);
+    }
+}
---
+++ Epoche.Shared/IListExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace Epoche.Shared;
+public static class IListExtensions
+{
+    public static bool RemoveFirst<T>(this IList<T> items, Func<T, bool> predicate)
+    {
+        var count = items.Count;
+        for (var x = 0; x < count; ++x)
+        {
+            if (predicate(items[x]))
+            {
+                items.RemoveAt(x);
+                return true;
+            }
+        }
+        return false;
+    }
+    public static int RemoveAll<T>(this IList<T> items, Func<T, bool> predicate)
+    {
+        var indexes = new List<int>();
+        var count = items.Count;
+        for (var x = 0; x < count; ++x)
+        {
+            if (predicate(items[x]))
+            {
+                indexes.Add(x);
+            }
+        }
+        for (var x = indexes.Count - 1; x >= 0; --x)
+        {
+            items.RemoveAt(indexes[x]);
+        }
+        return indexes.Count;
+    }
+}
