82c5e8bd261b82798b2d8d9fc2f7cdc4d8ecec0c
Josh <josh@joshlang.ca>
2022-02-10 16:45:25 -0800

    Adding ConcurrentActionRunner

---
+++ Epoche.Shared.Tests/ConcurrentActionRunnerTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Epoche.Shared;
+public class ConcurrentActionRunnerTests
+{
+    [Fact]
+    public async Task EmptyItems_DoesNothing() => await Array.Empty<int>().ToConcurrentActions(x => Task.CompletedTask, 32, default);
+
+    [Fact]
+    public async Task AllTasksExecuted()
+    {
+        var count = 0;
+        await Enumerable.Range(0, 100).ToConcurrentActions(x => { Interlocked.Increment(ref count); return Task.CompletedTask; }, 32, default);
+        Assert.Equal(100, count);
+    }
+
+    [Fact]
+    public async Task AllTasksExecuted_WithExceptions()
+    {
+        try
+        {
+            await Enumerable.Range(0, 100).ToConcurrentActions(x => { throw new Exception(); }, 32, default);
+        }
+        catch (AggregateException e)
+        {
+            Assert.Equal(100, e.InnerExceptions.Count);
+        }
+    }
+
+    [Fact]
+    public async Task CancelledBeforeExecution()
+    {
+        using var cts = new CancellationTokenSource();
+        cts.Cancel();
+        var count = 0;
+        await Assert.ThrowsAsync<OperationCanceledException>(() => Enumerable.Range(0, 100).ToConcurrentActions(x => { Interlocked.Increment(ref count); return Task.CompletedTask; }, 32, cts.Token));
+        Assert.Equal(0, count);
+    }
+
+    [Fact]
+    public async Task CancelsLongTasks()
+    {
+        using var cts = new CancellationTokenSource();
+        var count = 0;
+        await Assert.ThrowsAsync<OperationCanceledException>(() => Enumerable.Range(0, 100).ToConcurrentActions(x => { if (Interlocked.Increment(ref count) == 40) { cts.Cancel(); }; return Task.Delay(TimeSpan.FromSeconds(1)); }, 32, cts.Token));
+        Assert.True(count >= 40 && count <= 80);
+    }
+}
---
+++ Epoche.Shared/ConcurrentActionRunner.cs
@@ -0,0 +1,76 @@
+﻿namespace Epoche.Shared;
+public static class ConcurrentActionRunner
+{
+    public static async Task ToConcurrentActions<T>(this IEnumerable<T> items, Func<T, Task> createTask, int maxConcurrency, CancellationToken cancellationToken = default)
+    {
+        if (createTask is null) { throw new ArgumentNullException(nameof(createTask)); }
+        if (maxConcurrency <= 0) { throw new ArgumentOutOfRangeException($"{nameof(maxConcurrency)}={maxConcurrency}. Must be positive."); }
+        cancellationToken.ThrowIfCancellationRequested();
+        var exceptions = new List<Exception>();
+        var semaphore = new SemaphoreSlim(maxConcurrency);
+        var tasks = new HashSet<Task>();
+        Action<Task> release = t =>
+        {
+            lock (tasks)
+            {
+                tasks.Remove(t);
+            }
+            if (t.Exception is Exception e)
+            {
+                lock (exceptions)
+                {
+                    exceptions.Add(e);
+                }
+            }
+            semaphore.Release();
+        };
+        var stoppedEarly = false;
+        foreach (var item in items)
+        {
+            try
+            {
+                await semaphore.WaitAsync(cancellationToken);
+            }
+            catch
+            {
+                stoppedEarly = true;
+                break;
+            }
+            try
+            {
+                var task = createTask(item);
+                if (task.Status == TaskStatus.Created)
+                {
+                    task.Start();
+                }
+                lock (tasks)
+                {
+                    tasks.Add(task);
+                }
+                _ = task.ContinueWith(release, TaskContinuationOptions.ExecuteSynchronously);
+            }
+            catch (Exception e)
+            {
+                lock (exceptions)
+                {
+                    exceptions.Add(e);
+                }
+                semaphore.Release();
+            }
+        }
+        List<Task> outstandingTasks;
+        lock (tasks)
+        {
+            outstandingTasks = tasks.ToList();
+        }
+        await Task.WhenAll(outstandingTasks).ConfigureAwait(false);
+        if (stoppedEarly)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+        }
+        if (exceptions.Count > 0)
+        {
+            throw new AggregateException(exceptions);
+        }
+    }
+}
