4ba3b0df27c3a34e6a7950e4940aa37e3e4ad7ba
Josh <josh@joshlang.ca>
2022-03-04 07:56:21 -0800

    Adding ulong json converter

---
+++ Epoche.Shared.Tests/Json/UInt64ConverterTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text.Json;
+using Xunit;
+
+namespace Epoche.Shared.Json;
+
+public class UInt64ConverterTests
+{
+    static readonly JsonSerializerOptions JsonSerializerOptions = new()
+    {
+        Converters = { new UInt64Converter() }
+    };
+
+    class TestObj
+    {
+        public ulong A { get; set; }
+    }
+
+    [Fact]
+    public void Read_String_ReturnsValue()
+    {
+        var obj = JsonSerializer.Deserialize<TestObj>(@"{""A"":""123""}", JsonSerializerOptions);
+        Assert.Equal(123u, obj?.A);
+    }
+
+    [Fact]
+    public void Read_EmptyString_Throws() => Assert.ThrowsAny<Exception>(() => JsonSerializer.Deserialize<TestObj>(@"{""A"":""""}", JsonSerializerOptions));
+
+    [Fact]
+    public void Read_InvalidString_Throws() => Assert.ThrowsAny<Exception>(() => JsonSerializer.Deserialize<TestObj>(@"{""A"":""a""}", JsonSerializerOptions));
+
+    [Fact]
+    public void Read_NonString_Throws() => Assert.ThrowsAny<Exception>(() => JsonSerializer.Deserialize<TestObj>(@"{""A"":1}", JsonSerializerOptions));
+
+    [Fact]
+    public void Write_Roundtrips()
+    {
+        var test = new TestObj { A = 123 };
+        var s = JsonSerializer.Serialize(test, JsonSerializerOptions);
+        Assert.Contains(@"""123""", s);
+        var obj = JsonSerializer.Deserialize<TestObj>(s, JsonSerializerOptions);
+        Assert.Equal(obj?.A, test.A);
+    }
+}
---
+++ Epoche.Shared/Json/UInt64Converter.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Epoche.Shared.Json;
+
+public sealed class UInt64Converter : JsonConverter<ulong>
+{
+    public static readonly UInt64Converter Instance = new();
+
+    public override ulong Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.String)
+        {
+            throw new InvalidOperationException("Only string can be converted to ulong with this converter");
+        }
+
+        if (ulong.TryParse(reader.GetString(), out var value))
+        {
+            return value;
+        }
+
+        throw new FormatException("The value could not be parsed into a ulong");
+    }
+
+    public override void Write(Utf8JsonWriter writer, ulong value, JsonSerializerOptions options) => writer.WriteStringValue(value.ToString());
+}
