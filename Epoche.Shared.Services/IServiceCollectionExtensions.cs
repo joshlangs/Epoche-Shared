934ede01a45cafcc94325f799bcd857d6460e209
Josh <josh@joshlang.ca>
2022-09-26 08:34:34 -0700

    Adding Epoche.Shared.Services

---
+++ Epoche.Shared.Services/IServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Epoche.Shared.Services;
+public static class IServiceCollectionExtensions
+{
+    public static IServiceCollection AddSingletonHostedService<TServiceInterface, TService>(this IServiceCollection services)
+        where TServiceInterface : class
+        where TService : class, IHostedService, TServiceInterface
+        => services
+            .AddSingleton<TServiceInterface, TService>()
+            .AddHostedService<SingletonServiceWrapper<TServiceInterface>>();
+}
---
+++ Epoche.Shared.Services/SingletonServiceWrapper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Epoche.Shared.Services;
+
+sealed class SingletonServiceWrapper<TService> : IHostedService where TService : class
+{
+    readonly IHostedService HostedService;
+
+    public SingletonServiceWrapper(TService service)
+    {
+        HostedService = (service as IHostedService) ?? throw new InvalidOperationException($"{service?.GetType().Name} does not implement IHostedService");
+    }
+    public Task StartAsync(CancellationToken cancellationToken) => HostedService.StartAsync(cancellationToken);
+    public Task StopAsync(CancellationToken cancellationToken) => HostedService.StopAsync(cancellationToken);
+}
